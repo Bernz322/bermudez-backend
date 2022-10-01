b3a2ddcd9f6ba6a8132f90810e56792b53e04169
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-10-01 13:35:42 +0800

    fix sequelize config

--- models/AppointmentModel.js
+++ models/AppointmentModel.js
@@ -1,37 +1,37 @@
-module.exports = (sequelize, DataTypes)=>{
+module.exports = (sequelize, DataTypes) => {
     const AppointmentModel = sequelize.define(
-      "appointment",
-      {
-        ClientFirstname: {
-          type: DataTypes.STRING,
-          allowNull: false,
+        "appointment",
+        {
+            ClientFirstname: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            ClientMiddlename: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            ClientLastname: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            Address: {
+                type: DataTypes.STRING(1234),
+                allowNull: false,
+            },
+            AppointmentDate: {
+                type: DataTypes.STRING(1234),
+                allowNull: false,
+            },
+            AppointmentStatus: {
+                type: DataTypes.STRING,
+                defaultValue: "Pending",
+                allowNull: true,
+            },
         },
-        ClientMiddlename:{
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        ClientLastname:{
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        Address: {
-          type: DataTypes.STRING(1234),
-          allowNull: false,
-        },
-        AppointmentDate: {
-          type: DataTypes.STRING(1234),
-          allowNull: false,
-        },
-        AppointmentStatus: {
-          type: DataTypes.STRING,
-          defaultValue: "Pending",
-          allowNull: true,
-        },
-      },
-      {
-        timestamps: true,
-        freezeTableName: true,
-      }
+        {
+            timestamps: true,
+            freezeTableName: true,
+        }
     );
 
     return AppointmentModel;
